93668744d2e3597e8e19f7ba6bf6e9abe18a5e1b
wallace1 <317@lab.317.org>
2018-09-24 18:41:57 +0800

    algorithm mowanle

--- algorithm.py
+++ algorithm.py
@@ -1,18 +1,18 @@
 import math
-from colormath.color_objects import XYZColor, sRGBColor
+from colormath.color_objects import XYZColor, sRGBColor,LabColor
 from colormath.color_conversions import convert_color
-import colormath
-import pandas
-from re import split as sp
+from colormath.color_diff import delta_e_cie2000
 
 def change_space(rgb1code):
-    r = float(rgb1code[1:3])/255.0
-    g = float(rgb1code[3:5])/255.0
-    b = float(rgb1code[5:7])/255.0
-    rgb1 = colormath.color_objects.sRGBColor(r,g,b)
+    r = float(int(rgb1code[1:3],16))/255.0
+    g = float(int(rgb1code[3:5],16))/255.0
+    b = float(int(rgb1code[5:7],16))/255.0
+    rgb1 = sRGBColor(r,g,b)
     xyz = convert_color(rgb1, XYZColor,target_illuminant='d50')
+    lab = convert_color(xyz,LabColor)
+    return lab
 def different_color(c1,c2):
-
-    deltae = colormath.delta_e_cie2000(c1, c2, Kl=1, Kc=1, Kh=1)
+    c1 = change_space(c1)
+    c2 = change_space(c2)
+    deltae = delta_e_cie2000(c1,c2,2,1,1)
     return deltae
-print(different_color('#66ccff','#66ccff'))
